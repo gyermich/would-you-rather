4c12ffd261778850e8535e54b1bdee706fa73e92
galina <galina@energysavvy.com>
2018-10-19 20:29:30 -0700

    add logging middleware

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -5,8 +5,7 @@ import { setAuthedUser } from '../actions/authedUser'
 
 const AUTHED_ID = 'sarahedo'
 
-export function handleInitialData
- () {
+export function handleInitialData() {
     return (dispatch) => {
         return getInitialData()
         .then(({ users, questions }) => {
