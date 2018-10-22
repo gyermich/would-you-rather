bdf2b831bd71676d263218b801c7161bedd9da3c
galina <galina@energysavvy.com>
2018-10-22 15:17:29 -0700

    add 404 wuestion not found page

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { Grid, Row, Col, Image, Radio, FormGroup, Button, Media } from 'react-bootstrap'
 import { handleAnswerQuestion } from '../actions/questions'
+import QuestionNotFound from './QuestionNotFound'
 
 class Question extends Component {
   handleAnswer = (e, answer) => {
@@ -17,53 +18,71 @@ class Question extends Component {
   }
   render() {
     const { question, author } = this.props
-    const {
-        id, timestamp, optionOne, optionTwo
-    } = question
 
     return (
-      <div className='question'>
-      <Media>
-          <Media.Left>
-            <img
-                width={100}
-                height={100}
-                src={author.avatarURL}
-                alt={`Avatar of ${author.name}`}
-            />
-          </Media.Left>
-          <Media.Body>
-            <Media.Heading>{author.name} asks:</Media.Heading>
-            <Row>
-                <Button
-                  questionId={question.id}
-                  optionName="optionOne"
-                  onClick={(e) => this.handleAnswer(e, 'optionOne')}>
-                  {question.optionOne.text}
-                </Button>
-                <p>OR</p>
-                <Button
-                  questionId={question.id}
-                  optionName="optionTwo"
-                  onClick={(e) => this.handleAnswer(e, 'optionTwo')}>
-                  {question.optionTwo.text}
-                </Button>
-              </Row>
-          </Media.Body>
-        </Media>
+      <Fragment>
+        {question
+            ?
+            (<div className='question'>
+              <Media>
 
-      </div>
+                  <Media.Left>
+                    <img
+                        width={160}
+                        height={160}
+                        src={author.avatarURL}
+                        alt={`Avatar of ${author.name}`}
+                    />
+                  </Media.Left>
+
+                  <Media.Body>
+
+                    <Media.Heading>
+                    <p>{author.name} asks:</p>
+                    </Media.Heading>
+
+                    <Row><h5>Would you rather...?</h5></Row>
+
+                    <Row>
+                        <Button
+                          questionId={question.id}
+                          optionName="optionOne"
+                          onClick={(e) => this.handleAnswer(e, 'optionOne')}>
+                          {question.optionOne.text}
+                        </Button>
+
+                        <span className='option-divider'>OR</span>
+
+                        <Button
+                          questionId={question.id}
+                          optionName="optionTwo"
+                          onClick={(e) => this.handleAnswer(e, 'optionTwo')}>
+                          {question.optionTwo.text}
+                        </Button>
+                      </Row>
+
+                  </Media.Body>
+
+                </Media>
+              </div>
+            )
+            : <QuestionNotFound />}
+        </Fragment>
+
     )
   }
 }
+
  function mapStateToProps ({ authedUser, users, questions }, props) {
     const { id } = props.match.params
     const question = questions[id]
-    const author = users[question.author]
+    const author = question ? users[question.author] : ''
+
   return {
     authedUser,
     question,
     author,
   }
 }
+
  export default connect(mapStateToProps)(Question)
---
+++ src/components/QuestionNotFound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function QuestionNotFound () {
+    return (
+        <div>
+            <h1 className='text-center'>404: Question not found</h1>
+        </div>
+    )
+}
